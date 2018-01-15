118c3aee695c4bed804f0b8e32c819ebc2cf5477
Thomas Johnson <tbjohnson123@gmail.com>
2018-01-15 13:52:19 -0800

    Subscribers now have access to the action, and Action now has a Name which is automatically populated when using the new dispatchAction macro.

--- redux/redux.nim
+++ redux/redux.nim
@@ -1,7 +1,10 @@
+import macros
+
 type
   Action* = ref object of RootObj
+    Name*: string
   Reducer* [S] = proc (state: S = nil; action: Action = nil): S
-  Subscriber* [S] = proc (state: S)
+  Subscriber* [S] = proc (state: S, action: Action)
   Store [S] = ref object {.requiresInit.}
     state: S
     reducer: Reducer[S]
@@ -29,4 +32,32 @@ proc unsubscribe*[S](store: Store[S], subscriber: Subscriber[S]) =
 proc dispatch*(store: Store, action: Action) =
   store.state = store.reducer(state=store.state, action=action)
   for subscriber in store.subscribers:
-    subscriber(store.state)
+    subscriber(store.state, action)
+
+macro dispatchAction*(storename: Store, actiontypename: untyped): untyped =
+  result = newStmtList()
+
+  var varSection = newNimNode(nnkVarSection)
+  var identDefs = newNimNode(nnkIdentDefs)
+  var identNode1 = newIdentNode("action")
+  identDefs.add(identNode1)
+  var emptyNode = newNimNode(nnkEmpty)
+  identDefs.add(emptyNode)
+  var objConstr = newNimNode(nnkObjConstr)
+  var identNode2 = newIdentNode($actiontypename)
+  objConstr.add(identNode2)
+  var exprColonExpr = newNimNode(nnkExprColonExpr)
+  exprColonExpr.add(newIdentNode("Name"))
+  exprColonExpr.add(newStrLitNode($actiontypename))
+  objConstr.add(exprColonExpr)
+  identDefs.add(objConstr)
+  varSection.add(identDefs)
+  result.add(varSection)
+
+  var callNode = newNimNode(nnkCall)
+  var dotExprNode = newNimNode(nnkDotExpr)
+  dotExprNode.add(newIdentNode($storename))
+  dotExprNode.add(newIdentNode("dispatch"))
+  callNode.add(dotExprNode)
+  callNode.add(newIdentNode("action"))
+  result.add(callNode)
